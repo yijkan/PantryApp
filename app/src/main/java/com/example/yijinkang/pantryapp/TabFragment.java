0e84b99fc3356b527cb7212fa1800e0f31ecb197
Tracy Wan <tracymwan@gmail.com>
2016-09-17 16:32:50 -0400

    Added tabs

---
+++ app/src/main/java/com/example/yijinkang/pantryapp/TabFragment.java
@@ -0,0 +1,17 @@
+package com.example.yijinkang.pantryapp;
+
+    import android.os.Bundle;
+    import android.support.v4.app.Fragment;
+    import android.view.LayoutInflater;
+    import android.view.View;
+    import android.view.ViewGroup;
+
+/**
+ * Created by Admin on 11-12-2015.
+ */
+public class TabFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tabs, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/yijinkang/pantryapp/ViewPagerAdapter.java
@@ -0,0 +1,28 @@
+package com.example.yijinkang.pantryapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by Admin on 11-12-2015.
+ */
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return new TabFragment();    // Which Fragment should be dislpayed by the viewpager for the given position
+        // In my case we are showing up only one fragment in all the three tabs so we are
+        // not worrying about the position and just returning the TabFragment
+    }
+
+    @Override
+    public int getCount() {
+        return 3;           // As there are only 3 Tabs
+    }
+
+}
