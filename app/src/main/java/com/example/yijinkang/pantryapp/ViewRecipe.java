fd351555e8d046e6778fa227c5c7039a55e1c808
Aidan Fitzgerald <duckjr159@gmail.com>
2016-09-17 18:55:01 -0400

    starting the SQLite code

--- app/src/main/java/com/example/yijinkang/pantryapp/ViewRecipe.java
+++ app/src/main/java/com/example/yijinkang/pantryapp/ViewRecipe.java
@@ -1,15 +1,27 @@
 package com.example.yijinkang.pantryapp;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.SimpleCursorAdapter;
 
 import com.example.yijinkang.pantryapp.R;
 
 public class ViewRecipe extends AppCompatActivity {
+
+    private SQLiteHelper dbHelper;
+    private SimpleCursorAdapter dataAdapter;
+    SQLiteDatabase dbread;
+
+    // TODO: parent needs to send the recipe ID in the intent
+    private Intent parentIntent = getIntent();
+    int recipeID = parentIntent.getIntExtra("recipeID", -1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +38,10 @@ public class ViewRecipe extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        dbHelper = new SQLiteHelper(this);
+        dbread = dbHelper.getReadableDatabase();
+
+        Cursor resultSet = dbread.rawQuery("select * from " + dbHelper.TABLE_RECIPES + " where " + dbHelper.COLUMN_ID + " = " + recipeID, new String[0]);
     }
 }
