1efb7b31f2938bc47ccf4750d68452b9ff5aa91a
Yijin <yijinkang@gmail.com>
2016-09-17 22:42:15 -0400

    add button works but looks bad

--- app/src/main/java/com/example/yijinkang/pantryapp/ListRecipes.java
+++ app/src/main/java/com/example/yijinkang/pantryapp/ListRecipes.java
@@ -1,15 +1,15 @@
 package com.example.yijinkang.pantryapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class ListRecipes extends AppCompatActivity {
+public class ListRecipes extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +19,7 @@ public class ListRecipes extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(this);
     }
 
     @Override
@@ -48,5 +42,10 @@ public class ListRecipes extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(this, NewRecipe.class));
     }
 }
